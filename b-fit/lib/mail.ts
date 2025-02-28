9fed6b8f712e81db42e293732e9e1f80a2fbad43
Bartosz Zych <bartosz.j.zych@gmail.com>
2025-02-28 20:09:29 +0000

    Pushing live

--- b-fit/lib/mail.ts
+++ b-fit/lib/mail.ts
@@ -1,10 +1,11 @@
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const domain = process.env.NEXT_PUBLIC_APP_URL;
 export async function sendTwoFactorTokenEmail(email: string, token: string) {
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@b-fit.co",
     to: email,
     subject: "2FA Code",
     html: `<p>Here is your 2FA code: ${token}</p>`,
@@ -12,10 +13,10 @@ export async function sendTwoFactorTokenEmail(email: string, token: string) {
 }
 
 export async function sendResetPasswordEmail(email: string, token: string) {
-  const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+  const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@b-fit.co",
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${resetLink}">here</a> to reset your password.</p>`,
@@ -23,10 +24,10 @@ export async function sendResetPasswordEmail(email: string, token: string) {
 }
 
 export async function sendVerificationEmail(email: string, token: string) {
-  const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+  const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@b-fit.co",
     to: email,
     subject: "Confrim your email",
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
