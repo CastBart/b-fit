5c46f160ad2f8867f1d5b4eadb67566f3ddc3793
Bartosz Zych <bartosz.j.zych@gmail.com>
2025-03-06 19:59:19 +0000

    Individual Exercise Filter Responsive

--- b-fit/components/exercises/exercise-filter.tsx
+++ b-fit/components/exercises/exercise-filter.tsx
@@ -39,32 +39,34 @@ export default function ExerciseFilter<
   return (
     <Dialog>
       <DialogTrigger asChild className="grow">
-        <Button variant="default" className="text-xl">{title}</Button>
+        <Button variant="default" className="text-xl">
+          {title}
+        </Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-md">
+      <DialogContent className="custom-dialog">
         <DialogHeader className="gap-2">
           <DialogTitle className="text-center">{`${title} Filters`}</DialogTitle>
           <Separator className="h-1" />
-          <div className="max-h-[600px] grid grid-cols-2 gap-2 p-2 overflow-y-auto custom-scrollbar">
-            {data.map((item) => (
-              <div
-                key={String(item)}
-                onClick={() => toggleSelection(item)} // ✅ Clicking anywhere toggles
-                className="flex gap-2 items-center justify-center h-20 px-4 py-2 rounded-sm bg-primary text-primary-foreground shadow hover:bg-primary/90 cursor-pointer"
-              >
-                <Checkbox
-                  checked={selectedItems.includes(item)}
-                  onCheckedChange={() => toggleSelection(item)}
-                  id={String(item)}
-                  className="border-primary-foreground"
-                />
-                <label className="w-full cursor-pointer" htmlFor={String(item)}>
-                  {String(item)}
-                </label>
-              </div>
-            ))}
-          </div>
         </DialogHeader>
+        <div className=" grid grid-cols-2 gap-2 max-h-[500px] p-2 overflow-y-auto custom-scrollbar">
+          {data.map((item) => (
+            <div
+              key={String(item)}
+              onClick={() => toggleSelection(item)} // ✅ Clicking anywhere toggles
+              className="flex gap-2 items-center justify-center h-20 px-4 py-2 rounded-sm bg-primary text-primary-foreground shadow hover:bg-primary/90 cursor-pointer"
+            >
+              <Checkbox
+                checked={selectedItems.includes(item)}
+                onCheckedChange={() => toggleSelection(item)}
+                id={String(item)}
+                className="border-primary-foreground"
+              />
+              <label className="w-full cursor-pointer" htmlFor={String(item)}>
+                {String(item)}
+              </label>
+            </div>
+          ))}
+        </div>
         <DialogFooter className="sm:justify-start">
           <DialogClose asChild>
             <Button type="button" variant="secondary">
