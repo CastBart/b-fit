e817a1070010fe9b0dae4a7a9838e3c3142e9032
Bartosz Zych <bartosz.j.zych@gmail.com>
2025-08-06 08:41:05 +0100

    Create Exercise Server action cleaning code

--- b-fit/actions/create-exercise.ts
+++ b-fit/actions/create-exercise.ts
@@ -16,60 +16,7 @@ import { createExerciseDB } from "@/lib/db/exercise";
 export async function createExercise(
   values: z.infer<typeof CreateExerciseSchema>
 ) {
-  // // Validate fields
-  // const validatedFields = CreateExerciseSchema.safeParse(values);
-  // if (!validatedFields.success) {
-  //   return { error: "Invalid exercise fields!" };
-  // }
-
-  // const {
-  //   exerciseName,
-  //   equipment,
-  //   primaryMuscle,
-  //   auxiliaryMuscles,
-  //   exerciseType,
-  // } = validatedFields.data;
-
-  // Ensure the user is authenticated
-
-  // // Convert frontend enum values to Prisma enums
-  // const equipmentEnumKey = getEnumKeyByValue(ExerciseEquipment, equipment);
-  // const primaryMuscleEnumKey = getEnumKeyByValue(MuscleGroup, primaryMuscle);
-  // const auxiliaryMusclesEnumKeys = getEnumKeysByValues(
-  //   MuscleGroup,
-  //   auxiliaryMuscles
-  // );
-  // const exerciseTypeEnumKey = getEnumKeyByValue(ExerciseType, exerciseType);
-
-  // // Ensure all required fields are correctly mapped
-  // if (!equipmentEnumKey || !primaryMuscleEnumKey || !exerciseTypeEnumKey) {
-  //   return { error: "Invalid enum values provided." };
-  // }
-
-  // try {
-  //   // Create exercise in the database
-  //   await db.exercise.create({
-  //     data: {
-  //       name: exerciseName,
-  //       equipment: equipmentEnumKey,
-  //       primaryMuscle: primaryMuscleEnumKey,
-  //       auxiliaryMuscles: auxiliaryMusclesEnumKeys,
-  //       exerciseType: exerciseTypeEnumKey,
-  //       userId: session.user.id, // Linking exercise to the logged-in user
-  //       ownership: "Custom"
-  //     },
-  //   });
-
-  //   return {
-  //     exercise: {
-  //       exerciseName,
-  //       equipment,
-  //       primaryMuscle,
-  //       auxiliaryMuscles,
-  //       exerciseType,
-  //     },
-  //   };
-
+  
   try {
     const session = await auth();
     if (!session?.user?.id) {
@@ -78,7 +25,7 @@ export async function createExercise(
     const result = await createExerciseDB(values, session.user.id); // Use the user ID from the session
 
     if ("error" in result) {
-      return result; // { error: ... }
+      return result; 
     }
     return {
       exercise: {
