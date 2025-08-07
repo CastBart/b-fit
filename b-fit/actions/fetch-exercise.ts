c3ba058c986211c1a1105cb0f0da3fe95382cc19
Bartosz Zych <bartosz.j.zych@gmail.com>
2025-08-07 08:40:49 +0100

    Fetch exercise history changed to api route - with commented out code removed

--- b-fit/actions/fetch-exercise.ts
+++ b-fit/actions/fetch-exercise.ts
@@ -1,5 +1,4 @@
 // actions/fetch-exercise.ts
-import { db } from "@/lib/db/db";
 import { auth } from "@/auth";
 import { fetchExerciseHistoryDB } from "@/lib/db/exercise";
 
@@ -19,45 +18,7 @@ export interface ExerciseSet {
 export async function fetchExercise(
   exerciseId: string
 ): Promise<ExerciseHistory[] | { error: string }> {
-  // const session = await auth();
-  // if (!session?.user) {
-  //   return { error: "Unauthorised" };
-  // }
-  // const userId = session.user.id;
-  // const histories = await db.exerciseHistory.findMany({
-  //   where: {
-  //     exerciseId,
-  //     userId,
-  //   },
-  //   include: {
-  //     sets: true,
-  //     session: {
-  //       select: {
-  //         startTime: true,
-  //         workoutName: true,
-  //       },
-  //     },
-  //     exercise: {
-  //       select: {
-  //         name: true,
-  //       },
-  //     },
-  //   },
-  //   orderBy: {
-  //     createdAt: "asc",
-  //   },
-  // });
-
-  // return histories.map((history) => ({
-  //   exerciseName: history.exercise.name,
-  //   workoutName: history.session.workoutName,
-  //   sessionStartTime: history.session.startTime,
-  //   sets: history.sets.map((set) => ({
-  //     reps: set.reps,
-  //     weight: set.weight,
-  //     setNumber: set.setNumber,
-  //   })),
-  // }));
+  
   try {
     const session = await auth();
     if (!session?.user || !session.user.id) {
