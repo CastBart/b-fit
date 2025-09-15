8c200ba0f22fe22b0c0fcd486fee0d0cc6028ac0
Bartosz Zych <bartosz.j.zych@gmail.com>
2025-09-15 21:16:52 +0100

    Session Persister

--- b-fit/store/index.ts
+++ b-fit/store/index.ts
@@ -1,12 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sessionReducer from "./sessionSlice";
-import timerReducer from "./timerSlice"
+import timerReducer from "./timerSlice";
 
 export const store = configureStore({
   reducer: {
     session: sessionReducer,
     timer: timerReducer,
   },
+  middleware: (getDefault) =>
+    getDefault({
+      serializableCheck: {
+        ignoredActions: ["session/rehydrateSession"], // ✅ allow hydration payloads
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
